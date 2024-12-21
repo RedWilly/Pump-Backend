45cac8089cd761afb8105e697566a7548332f921
RedWilly <thompsonau023@gmail.com>
2024-12-21 20:06:26 +0000

    Allow undefined origin in  development only

--- src/app.ts
+++ src/app.ts
@@ -41,10 +41,16 @@ const corsOptions = {
   origin: function (origin: string | undefined, callback: (error: Error | null, allow?: boolean) => void) {
     console.log('Incoming request from origin:', origin);
 
-    if (!origin) {
-      callback(null, true);
-      return;
+    // Allow undefined origin only in development
+    if (!origin && process.env.NODE_ENV === 'development') {
+        callback(null, true);
+        return;
     }
+    
+    if (!origin) {
+         callback(new Error(`CORS error: Origin '${origin}' is not allowed.`));
+        return;
+     }
     
       const isAllowed = allowedOrigins.some(allowedOrigin => {
           if (allowedOrigin.includes('*')){
